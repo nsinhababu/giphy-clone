15603e22561e3b9713bf91d59afc99f2fd706c83
nilanjan <nsinhababu@outlook.com>
2022-02-01 19:19:01 +0530

    done css for search gifs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const search = document.querySelector('.search__input');
-const searchImgBtn = document.querySelector('.search__img');
+const searchImgBtn = document.querySelector('.search__logo');
 const gifContainer = document.querySelector('.search-container');
 
 // search.addEventListener('key', (e) => {
@@ -13,22 +13,8 @@ const gifContainer = document.querySelector('.search-container');
 //   });
 const API_KEY = 'RgWANDqJD2RKTXDgnd6PqDKyGjXNNsd7';
 
-searchImgBtn.addEventListener('click', (e) => {
-  const updatedParams = search.value.replaceAll(' ', '+');
-  const searchUrl = `http://api.giphy.com/v1/gifs/search?q=${updatedParams}&api_key=${API_KEY}&limit=12`;
-  fetchApi(searchUrl);
-});
-
 // const searchUrl = 'http://api.giphy.com / v1 / gifs / search?';
 
-function fetchApi(url) {
-  fetch(url)
-    .then((response) => response.json())
-    .then((gifsData) => {
-      // console.log(data.data.map((item) => item.images.fixed_height.url));
-      processImages(gifsData.data);
-    });
-}
 function processImages(gifs) {
   const gifsArr = (gifs || []).map((gif) => {
     const imageUrl = gif.images.fixed_height.url;
@@ -36,6 +22,31 @@ function processImages(gifs) {
   });
   gifContainer.innerHTML = gifsArr;
 }
+
+function fetchSearchApi(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((gifsData) => {
+      // console.log(data.data.map((item) => item.images.fixed_height.url));
+      processImages(gifsData.data);
+    });
+}
+
+searchImgBtn.addEventListener('click', (e) => {
+  const updatedParams = search.value.replaceAll(' ', '+');
+  const searchUrl = `http://api.giphy.com/v1/gifs/search?q=${updatedParams}&api_key=${API_KEY}&limit=12`;
+  fetchSearchApi(searchUrl);
+});
+
+const trendingUrl = `http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=4`;
+
+function fetchTrendingApi() {
+  fetch(trendingUrl)
+    .then((response) => response.json())
+    .then((trending) => console.log(trending));
+}
+fetchTrendingApi();
+
 // function onSearchValue(e) {
 //   console.log(e.target.value);
 // }
